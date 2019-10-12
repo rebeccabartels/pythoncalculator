bf651350991e2a7b8098b9458ae8144c1658e558
Rebecca Bartels <rebeccakbartels@gmail.com>
2019-10-12 11:58:11 -0400

    Update operationchooser.py

--- operationchooser.py
+++ operationchooser.py
@@ -1,3 +1,5 @@
+
+
 def addition(x, y):
     outcome = x+y
     print(str(x) + "+" + str(y) + "=" + str(outcome))
@@ -5,11 +7,12 @@ def addition(x, y):
 
 def begin():
 
-    operation_chosen = str(input("What operation would you like to use? "))
-    x = int(input("What number do you want to use? "))
-    y = int(input("choose another number "))
+    operation_chosen = str(input(
+        "What operation would you like to use? \n[1]Addition\n[2]Subtraction\n[3]Multiplication\n[4]Division\nEnter Choice: "))
+    x = int(input("Choose a number "))
+    y = int(input("Choose another number "))
 
-    if (operation_chosen == "addition"):
+    if (operation_chosen == "[1]"):
 
         addition(x, y)
 
